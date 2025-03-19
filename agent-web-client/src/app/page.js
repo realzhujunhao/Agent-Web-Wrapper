554cb86f6f1647b0baaec161079d63cae77e578b
Junhao Zhu <realzhujunhao@gmail.com>
2025-03-19 13:26:13 +0800

    cookie jwt

--- agent-web-client/src/app/page.js
+++ agent-web-client/src/app/page.js
@@ -1,6 +1,58 @@
+"use client"
 import ChatComponent from "@/components/ChatComponent";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const serverDomain = "http://localhost:8085";
+    const [token, setToken] = useState(null);
+    const testAndSetToken = async () => {
+        const existingToken = Cookies.get("jwt");
+        if (existingToken) {
+            setToken(existingToken);
+            console.log("debug: cookie exists");
+            console.log(existingToken);
+            // reset expire time
+            Cookies.set("jwt", existingToken, { expires: 30 });
+        } else {
+            console.log("debug: cookie does not exist");
+            const resp = await fetch(`${serverDomain}/init-session`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(null),
+            });
+            if (!resp.ok) {
+                alert(`Error: Init session response status ${resp.status}`);
+                return;
+            }
+            const result = await resp.json();
+            console.log("debug: receive response from init-session");
+            console.log(result);
+
+            if (result.success) {
+                if (result.data) {
+                    Cookies.set("jwt", result.data, { expires: 30 });
+                    setToken(result.data);
+                    console.log("debug: set cookie")
+                    console.log(result.data);
+                }
+            } else {
+                // unreachable!
+                alert(`Error: Init session server error ${result.err}`);
+            }
+        }
+    };
+
+    useEffect(() => {
+        testAndSetToken();
+    }, []);
+
+    useEffect(() => {
+        console.log(`debug: now token is ${token}`);
+    }, [token]);
+
     return (
         <ChatComponent />
     );
